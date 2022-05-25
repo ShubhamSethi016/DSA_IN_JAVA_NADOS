d255ec4cda15fa03eabfdad35f1f811a892e5776
Shubham Sethi <shubhamsethi12345678@gmail.com>
2022-05-26 03:18:29 +0530

    Added ZigZag

---
+++ Recursion_Level1/PrintZigZag.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrintZigZag {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+        scn.close();
+    }
+
+    public static void pzz(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n + " ");
+        pzz(n-1);
+        System.out.print(n + " ");
+    }
+
+}
+
+// TC = O(3N)
+// SC = O(1)
