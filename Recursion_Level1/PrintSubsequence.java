1b87119942f560273cc92ec1cb1e67d648740bb6
Shubham Sethi <shubhamsethi12345678@gmail.com>
2022-06-01 23:25:26 +0530

    Added Print Subsequence Recursion

---
+++ Recursion_Level1/PrintSubsequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrintSubsequence {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        printSS(str, "");
+        scn.close();
+    }
+
+    public static void printSS(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        
+        char ch = str.charAt(0);
+        String subString = str.substring(1);
+        printSS(subString,ans + ch);
+        printSS(subString,ans + "");      
+    }
+
+}
