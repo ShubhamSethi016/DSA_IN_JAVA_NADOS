bd066513074e46186129af66c45aa4e78bc92536
Shubham Sethi <shubhamsethi12345678@gmail.com>
2022-06-16 01:24:55 +0530

    Added GenericTree Questions

---
+++ Generic_Tree/NodeToRootPath.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class NodeToRootPath {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+ public static ArrayList<Integer> nodeToRootPath(Node node, int data){
+    // write your code here
+    if(node.data == data){
+      ArrayList<Integer> temp = new ArrayList<>();
+      temp.add(node.data);
+      return temp;
+    }
+    ArrayList<Integer> path = new ArrayList<>(); 
+    for(Node child: node.children){
+      path = nodeToRootPath(child, data);
+      if(path.size()>0){
+        path.add(node.data);
+        return path;
+      }
+    }
+
+    return path;
+ }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int data = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    ArrayList<Integer> path = nodeToRootPath(root, data);
+    System.out.println(path);
+    // display(root);
+  }
+
+}
+
+// TC = O(N)
+// SC = O(logN) as at max space used by arraylist will be the elements at the depth(height) of tree.
